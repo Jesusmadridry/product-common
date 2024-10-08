7bf9f427787cd2e992054eeadbaa82e9a90e17c5
Jesus Madrid <114009973+JesusMadridMCH@users.noreply.github.com>
2024-10-08 13:20:27 -0500

    task_01-Product-Common: Including annotations @CreatedBy &  @LastModifiedBy, to resolve based on which user was authenticated

--- product-common-persist/src/main/java/com/common/persist/CommonEntity.java
+++ product-common-persist/src/main/java/com/common/persist/CommonEntity.java
@@ -1,5 +1,6 @@
 package com.common.persist;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 
 import java.time.LocalDateTime;
 
@@ -18,8 +21,14 @@ import java.time.LocalDateTime;
 public abstract class CommonEntity {
     @CreationTimestamp
     protected LocalDateTime createdDateTs;
+
+    @CreatedBy
+    @Column(updatable = false)
     protected String createdByUser;
+
     @UpdateTimestamp
     protected LocalDateTime modifiedDateTs;
+
+    @LastModifiedBy
     protected String modifiedByUser;
 }
