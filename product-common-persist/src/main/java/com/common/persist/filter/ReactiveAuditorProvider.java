a96930cfc680c8094640e09d3fc5188ac778a4eb
Jesus Madrid <114009973+JesusMadridMCH@users.noreply.github.com>
2024-10-08 13:19:20 -0500

    task_01-Product-Common: Creating authentication filter and reactive auditor provider resolve the modifyUser and created user by the annotation @CreatedBy &  @LastModifiedBy

---
+++ product-common-persist/src/main/java/com/common/persist/filter/ReactiveAuditorProvider.java
@@ -0,0 +1,27 @@
+package com.common.persist.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+
+import java.util.Optional;
+
+@Slf4j
+public class ReactiveAuditorProvider implements AuditorAware<String> {
+    private final InheritableThreadLocal<Authentication> authenticationRef = new InheritableThreadLocal<>();
+
+    public void registerAuthenticated(Authentication authentication) {
+        Optional.of(authentication)
+                .filter(Authentication::isAuthenticated)
+                .ifPresentOrElse(authenticationRef::set, authenticationRef::remove);
+    }
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        log.info("Get Current Auditor {}", authenticationRef.get());
+        var userName = Optional.ofNullable(authenticationRef.get())
+                .map(Authentication::getName)
+                .orElse(null);
+        return Optional.ofNullable(userName);
+    }
+}
